2018b78bca44040c24c61d94a94b0595d64adfa7
Ngo Tuan Dung <zun1903@gmail.com>
2020-08-05 16:38:40 +0700

    add bitstamp

--- exchanges.py
+++ exchanges.py
@@ -1,20 +1,34 @@
-import time
 import argparse
-import dateutil.parser
-
 from pyspark.sql import SQLContext, SparkSession
 from pyspark.sql.column import Column, _to_java_column
 import pyspark.sql.types as t
 import pyspark.sql.functions as F
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--read-topic', help='the kafka topic to read data from.')
+parser.add_argument('-s', '--sink-topic', help='the kafka topic to sink data to.')
+parser.add_argument('-b', '--broker', default='kafka:29092', help='the kafka bootstrap server.')
+parser.add_argument('-u', '--schema-registry-url', default='http://schema-registry:8081', help='the schema registry server.')
+parser.add_argument('-w', '--window', help='aggregrate window.')    
+
+# Parse arguments.
+args = parser.parse_args()
+schema_registry_url = args.schema_registry_url
+read_topic = args.read_topic
+sink_topic = args.sink_topic
+kafka_broker = args.broker
+window = args.window
+
+
 spark = SparkSession \
         .builder \
         .appName('structuredStreamingKafka') \
-        .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0,za.co.absa:abris_2.11:3.2.1,org.apache.avro:avro:1.9.1') \
-        .config('schemaRegistryUrl', 'http://schema-registry:8081') \
-        .config('schemaRegistryReadTopic', 'coins_realtime_update') \
-        .config('schemaRegistryWriteTopic', 'coins_hbase_sink') \
+        .config('schemaRegistryUrl', schema_registry_url) \
+        .config('kafkaBroker', kafka_broker) \
+        .config('readTopic', read_topic) \
+        .config('writeTopic', sink_topic) \
+        .config('window', window) \
         .getOrCreate()
 
 sc = spark.sparkContext
@@ -23,14 +37,6 @@ sqlc = SQLContext(sc)
 
 
 def from_avro(col):
-    """
-    avro deserialize
-
-    :param col: column name "key" or "value"
-    :param topic: kafka topic
-    :param schema_registry_url: schema registry http address
-    :return:
-    """
     jvm_gateway = sc._active_spark_context._gateway.jvm
     abris_avro = jvm_gateway.za.co.absa.abris.avro
     naming_strategy = getattr(
@@ -40,7 +46,7 @@ def from_avro(col):
 
     schema_registry_config_dict = {
         "schema.registry.url": spark.conf.get("schemaRegistryUrl"),
-        "schema.registry.topic": spark.conf.get("schemaRegistryReadTopic"),
+        "schema.registry.topic": spark.conf.get("readTopic"),
         "{col}.schema.id".format(col=col): "latest",
         "{col}.schema.naming.strategy".format(col=col): naming_strategy
     }
@@ -53,13 +59,6 @@ def from_avro(col):
 
 
 def to_avro(col):
-    """
-    avro  serialize
-    :param col: column name "key" or "value"
-    :param topic: kafka topic
-    :param schema_registry_url: schema registry http address
-    :return:
-    """
     jvm_gateway = sc._active_spark_context._gateway.jvm
     abris_avro = jvm_gateway.za.co.absa.abris.avro
     naming_strategy = getattr(
@@ -69,7 +68,7 @@ def to_avro(col):
 
     schema_registry_config_dict = {
         "schema.registry.url": spark.conf.get("schemaRegistryUrl"),
-        "schema.registry.topic": spark.conf.get("schemaRegistryWriteTopic"),
+        "schema.registry.topic": spark.conf.get("writeTopic"),
         "{col}.schema.id".format(col=col): "latest", 
         "{col}.schema.naming.strategy".format(col=col): naming_strategy
     }
@@ -82,48 +81,26 @@ def to_avro(col):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--topic-name', help='the kafka topic push to.')
-    # kafka's location, easily handle to remote kafka
-    parser.add_argument('-b', '--broker', help='the kafka topic push to.')
-    parser.add_argument('-w', '--window', help='aggregrate window.')
-    parser.add_argument('-m', '--watermark', help='watermark.')
-    parser.add_argument('--table-name', help='hbase table.')
-    
-
-    # Parse arguments.
-    args = parser.parse_args()
-    topic_name = args.topic_name
-    kafka_broker = args.broker
-    window = args.window
-    watermark = args.watermark
-    table_name = args.table_name
-
     df = spark \
       .readStream \
       .format('kafka') \
-      .option('kafka.bootstrap.servers', kafka_broker) \
+      .option('kafka.bootstrap.servers', spark.conf.get('kafkaBroker')) \
       .option('startingOffsets', 'latest') \
-      .option('subscribe', topic_name) \
+      .option('subscribe', spark.conf.get('readTopic')) \
       .load() \
       .select(from_avro('value').alias('parsed')) \
       .select('parsed.*')
-    
-#     query = df.writeStream.format('console').option('truncate', 'false').start().awaitTermination()
-    
-#     json_schema = '{"schema": {"type": "struct", "name": "coins.ohlc", "fields": [{"field": "exchange", "type": "string", "optional": false}, {"field": "symbol", "type": "string", "optional": false}, {"field": "time", "type": "string", "optional": false}, {"field": "open", "type": "float", "optional": false}, {"field": "high", "type": "float", "optional": false}, {"field": "low", "type": "float", "optional": false}, {"field": "close", "type": "float", "optional": false}]}, '
-    
+
     query = df \
       .withColumn('timestamp', F.col('time').cast('timestamp')) \
-      .withWatermark('timestamp', watermark) \
+      .withWatermark('timestamp', '1 minute') \
       .groupBy(F.window('timestamp', window), 'exchange', 'symbol') \
       .agg(F.first('price').alias('open'),
            F.max('price').alias('high'),
            F.min('price').alias('low'),
            F.last('price').alias('close'),
-           F.last('base_vol_24h').alias('base_vol_24h')) \
+           F.sum('volume').alias('volume')) \
       .withColumn('time', F.unix_timestamp('window.end').cast('string')) \
-      .withColumn('volume', F.col('base_vol_24h') * F.col('close')) \
       .withColumn('row_id', F.concat(
         'symbol', F.lit('#'), 'exchange', F.lit('#'), 'time')) \
       .withColumn('value', F.struct(
@@ -131,8 +108,8 @@ if __name__ == '__main__':
       .select(to_avro('value').alias('value')) \
       .writeStream \
       .format('kafka') \
-      .option('kafka.bootstrap.servers', kafka_broker) \
-      .option('topic', 'coins_hbase_sink') \
-      .option('checkpointLocation', "/tmp/checkpoint") \
+      .option('kafka.bootstrap.servers', spark.conf.get('kafkaBroker')) \
+      .option('topic', spark.conf.get("writeTopic")) \
+      .option('checkpointLocation', "hdfs://hdfs-namenode:8020/tmp/checkpoint") \
       .start() \
       .awaitTermination()
